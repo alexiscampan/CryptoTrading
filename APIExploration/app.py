24decfa73572209eca8397a76a3bb197b35ce8fc
alexiscampan <alexis.campan@ut-capitole.fr>
2021-11-11 19:56:58 +0100

    [dashboard] First approach

---
+++ APIExploration/app.py
@@ -0,0 +1,103 @@
+import dash
+from dash.dependencies import Input, Output
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly.express as px
+import pandas as pd
+from connection import one_day_curve, infos
+import dash_bootstrap_components as dbc
+import dash_table
+import json
+
+
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.CYBORG])
+
+# assume you have a "long-form" data frame
+# see https://plotly.com/python/px-arguments/ for more options
+
+app.layout = html.Div(children=[
+    html.H1(children='Crypto currency dashboard'),
+
+    html.Div([
+        dbc.Row([
+            dbc.Col(dcc.Dropdown(
+                id='currency',
+                options=[
+                    {'label': 'ETH', 'value': 'ETHUSDT'},
+                    {'label': 'BNB', 'value': 'BNBUSDT'},
+                    {'label': 'XRP', 'value': 'XRPUSDT'},
+                    {'label': 'XRP', 'value': 'XRPUSDT'},
+                    {'label': 'LTC', 'value': 'LTCUSDT'},
+                    {'label': 'DOT', 'value': 'DOTUSDT'},
+                    {'label': 'VET', 'value': 'VETUSDT'},
+                    {'label': 'BTC', 'value': 'BTCUSDT'}
+                ],
+                value='BTCUSDT'
+            )),
+            dbc.Col(dcc.Dropdown(
+                id='time',
+                options=[
+                    {'label':'minutes', 'value':'15 minutes ago UTC'},
+                    {'label':'hour', 'value':'1 hour ago UTC'},
+                    {'label':'day', 'value':'1 day ago UTC'},
+                    {'label':'week', 'value':'1 week ago UTC'},
+                    {'label':'month', 'value':'1 month ago UTC'},
+                ],
+                value='1 day ago UTC'
+            ))
+        ])
+    ]),
+    
+    html.Br(),
+
+    html.Div([
+        dbc.Row([
+            dbc.Col(html.Div(id='one_day')),
+            dbc.Col(html.Div(id='infos'))
+        ])
+    ]),
+
+    
+])
+
+@app.callback(
+    Output(component_id='one_day', component_property='children'),
+    [Input(component_id='currency', component_property='value'),
+    (Input(component_id='time', component_property='value'))]
+)
+def update_graph_one_day(value, time):
+    return dcc.Graph(figure=one_day_curve(value, time))
+
+@app.callback(
+    Output(component_id='infos', component_property='children'),
+    Input(component_id='currency', component_property='value')
+)
+def info(value):
+
+    df = pd.DataFrame.from_dict(json.loads(infos(value)), orient='index').transpose()
+    df = df[['baseAsset','status', 'orderTypes','permissions']]
+    return [dash_table.DataTable(
+            style_cell={
+                'overflow': 'hidden',
+                'textOverflow': 'ellipsis',
+                'maxWidth': 0
+            },
+            style_header={
+                'backgroundColor':'blue',
+                'fontWeight':'bold'
+            },
+            tooltip_data=[
+                {
+                    column: {'value': str(value), 'type': 'markdown'}
+                    for column, value in row.items()
+                } for row in df.to_dict('records')
+            ],
+            tooltip_delay=0,
+            tooltip_duration=None,
+            data=df.to_dict('rows'),
+            columns=[{"name": i, "id": i,} for i in (df.columns)]
+            )]
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
--- APIExploration/connection.py
+++ APIExploration/connection.py
@@ -1,17 +1,49 @@
-from binance import Client, ThreadedWebsocketManager, ThreadedDepthCacheManager
+from binance import Client, ThreadedWebsocketManager, ThreadedDepthCacheManager, AsyncClient, BinanceSocketManager
 from secret import key, secret
 import plotly.express as px
+import plotly.graph_objects as go
+import numpy as np
 import logging
 import pandas as pd
+import os
+import json
+from datetime import datetime
+import asyncio
+
 
 def connection():
-    client = Client(key, secret)
     logging.info('Connection successful')
-    klines = pd.DataFrame(client.get_historical_klines("BNBBTC", Client.KLINE_INTERVAL_1MINUTE, "1 day ago UTC"), columns=['open_time','open','high','low','close','volume','close_time','quote_asset_volume','number_of_trades','taker_buy_base_asset_volume', 'taker_buy_quote_aset_volume','ignore'])
+    os.system('python APIExploration/app.py')
+    
+
+def one_day_curve(currency, time):
+    client = Client(key, secret)
+    klines = pd.DataFrame(client.get_historical_klines(currency, Client.KLINE_INTERVAL_1MINUTE, time), columns=['open_time','open','high','low','close','volume','close_time','quote_asset_volume','number_of_trades','taker_buy_base_asset_volume', 'taker_buy_quote_aset_volume','ignore'])
     klines['open_time'] = pd.to_datetime(klines['open_time']/1000, unit='s', utc=True)
-    fig = px.line(klines,x='open_time',y='high')
-    return fig.show()
+    klines['percentage'] = (klines['close'].astype(float) - klines['open'].astype(float))*100
+    klines['mean'] = (klines['open'].astype(float)+klines['close'].astype(float))/2
+    fig = px.line(klines,x='open_time',y='mean', title='avg price for '+currency)
+    return fig
+
+def infos(currency):
+    client = Client(key, secret)
+    return json.dumps(client.get_symbol_info(currency))
+
+async def view():
+    client = await AsyncClient.create()
+    bm = BinanceSocketManager(client, user_timeout=60)
+    ts = bm.trade_socket('BTCUSDT')
+    # start any sockets here, i.e a trade socket
+    # ts = bm.miniticker_socket(1000)
+    # then start receiving messages
+    async with ts as tscm:
+        while True:
+            res = await tscm.recv()
+            print(res)
     
 
 if __name__ == '__main__':
     connection()
+    # infos('BTCUSDT')
+    # loop = asyncio.get_event_loop()
+    # loop.run_until_complete(view())
